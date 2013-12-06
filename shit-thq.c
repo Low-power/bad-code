108f3b4ff4b9fe6aed62c9018e11b1a5f745b922
WHR <msl0000023508@gmail.com>
2013-12-06 09:49:20 +0800

    Update 1

--- shit-thq.c
+++ shit-thq.c
@@ -3,7 +3,6 @@ Warning: BAD CODE!
 This code is only for testing compilers warning information
 You need to turn on all the warning of your compiler (ex. -Wall for gcc)
 
-
 警告：有害代码！
 此代码仅用以测试编译器的警告信息
 你需要启用编译器上的所有警告信息（例：gcc 加 -Wall 参数）
@@ -12,15 +11,14 @@ You need to turn on all the warning of your compiler (ex. -Wall for gcc)
 #include <stdio.h>
 
 int main(){
-	int  a=12;
-	a+=a-=a*a;
+	int a=12;
+	a+=a-=a*a;	// WRONG: Sequence point
 	printf("a = %d\n", a);
 	return 0;
 }
 
 /*
 此代码来自《C程序设计》（谭浩强 著，清华大学出版社 2005年7月第3版）， 第64页
-
 
 附部分编译器的警告信息：
 
