07ed449d0d8dca432910e0e4a16e57e3db7c48a5
WHR <msl0000023508@gmail.com>
2013-12-07 18:46:47 +0800

    Update 3

--- 978-7-900491-74-9.2001-8.1.2.p33.test-1.c
+++ 978-7-900491-74-9.2001-8.1.2.p33.test-1.c
@@ -2,13 +2,13 @@
 Warning: BAD CODE!
 This code is only for testing compilers warning information
 You need to turn on all the warning of your compiler (ex. -Wall for gcc)
-Why it is bad? Between the previous and next sequence point, a value of the same variable is changed several times, the behavior is undefined. See ISO/IEC 9899:1990, 3.3
+Why it is bad? Between the previous and next sequence point, a value of the same variable is changed several times, the behavior is undefined. See ISO/IEC 9899:1990, 6.3; and ISO/IEC 9899:1999，6.5
 Therefore, this coding should be strictly ban!
 
 警告：有害代码！
 此代码仅用以测试编译器的警告信息
 你需要启用编译器上的所有警告信息（例：gcc 加 -Wall 参数）
-为什么有害？在两个序点之间，同一变量的值被多次改变，其行为是未定义的。参见 ISO/IEC 9899:1990，3.3
+为什么有害？在相邻的两个序点之间，同一变量的值被多次改变，其行为是未定义的。参见 ISO/IEC 9899:1990，6.3；和 ISO/IEC 9899:1999，6.5
 因此，这种编码应严格禁止！
 */
 
@@ -16,7 +16,7 @@ Therefore, this coding should be strictly ban!
 
 int main(){
 	int a = 10, b = 10;
-	a += b -= a *= b /= 3;	// WRONG: Sequence point
+	a += b -= a *= b /= 3;	// Wrong code here
 	printf("a = %d, b = %d", a, b);
 	putchar('\n');
 	return 0;
--- shit-thq.c
+++ shit-thq.c
@@ -2,13 +2,13 @@
 Warning: BAD CODE!
 This code is only for testing compilers warning information
 You need to turn on all the warning of your compiler (ex. -Wall for gcc)
-Why it is bad? Between the previous and next sequence point, a value of the same variable is changed several times, the behavior is undefined. See ISO/IEC 9899:1990, 3.3
+Why it is bad? Between the previous and next sequence point, a value of the same variable is changed several times, the behavior is undefined. See ISO/IEC 9899:1990, 6.3; and ISO/IEC 9899:1999，6.5
 Therefore, this coding should be strictly ban!
 
 警告：有害代码！
 此代码仅用以测试编译器的警告信息
 你需要启用编译器上的所有警告信息（例：gcc 加 -Wall 参数）
-为什么有害？在两个序点之间，同一变量的值被多次改变，其行为是未定义的。参见 ISO/IEC 9899:1990，3.3
+为什么有害？在相邻的两个序点之间，同一变量的值被多次改变，其行为是未定义的。参见 ISO/IEC 9899:1990，6.3；和 ISO/IEC 9899:1999，6.5
 因此，这种编码应严格禁止！
 */
 
@@ -16,13 +16,14 @@ Therefore, this coding should be strictly ban!
 
 int main(){
 	int a=12;
-	a+=a-=a*a;	// WRONG: Sequence point
+	a+=a-=a*a;	// Wrong code here
 	printf("a = %d\n", a);
 	return 0;
 }
 
 /*
-此代码来自《C程序设计》（谭浩强 著，清华大学出版社 2005年7月第3版）；第64页
+此代码来自《C程序设计》（谭浩强 著，清华大学出版社 2005年7月 第3版，ISBN 978-7-302108-53-5），3.9，4；第64页。
+也存在于再版《C程序设计》（谭浩强 著，清华大学出版社 2010年6月 第4版，ISBN 978-7-302224-46-4），3.3.2，3；第62页。
 
 附部分编译器的警告信息：
 
